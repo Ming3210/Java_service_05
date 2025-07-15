fa928e1fa72d7bc3637c684965529c2382a72bab
unknown <vminh0227@gmail.com>
2025-07-15 11:24:17 +0700

    add

--- src/main/java/ra/java_servcie_05/controller/ProductController.java
+++ src/main/java/ra/java_servcie_05/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-public class ProductRestController {
+public class ProductController {
 
     @Autowired
     private ProductService productService;
