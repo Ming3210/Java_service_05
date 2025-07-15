0414f9cc8729ddc49e05a603a1c9222f855a3453
unknown <vminh0227@gmail.com>
2025-07-15 11:26:29 +0700

    add

---
+++ src/main/java/ra/java_servcie_05/model/response/DataResponse.java
@@ -0,0 +1,14 @@
+package ra.java_servcie_05.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataResponse<T> {
+    private T data;
+    private HttpStatus httpStatus;
+}
